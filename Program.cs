d50090745d8332d93072f7d8d7737cbeecfa4022
manunasci <nascimentomanuella73@gmail.com>
2021-09-17 20:57:40 -0300

    Adiciona o programa que converte a medida colocada pelo usuario

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Medidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a medida em metros que deseja: ");
+            string valordaMedida = Console.ReadLine();
+            double numerodaMedida = Double.Parse(valordaMedida);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Medidas em cm: " + numerodaMedida*100);
+            Console.WriteLine("Medidas em m: " + numerodaMedida);
+            Console.WriteLine("Medidas em km: " + numerodaMedida*0.001);
+        }
+    }
+}
